a94cce6983ce8bfbf075d391d64b4a1a416e0729
lpaeb <115804646+lpaeb@users.noreply.github.com>
2022-11-28 10:26:00 +0000

    Update FullscreenModal.php

--- FullscreenModal.php
+++ FullscreenModal.php
@@ -15,23 +15,37 @@ use yii\bootstrap5\Modal;
 class FullscreenModal extends Modal
 {
     public $modalBodyPadding= '20px;';
+    public $notitle = false;
 
     /**
      * Initializes the widget.
      */
     public function init()
     {
-
+    
        $this->options = array_merge([
            'class' => 'modal-fs fade',
        ], $this->options);
-
+       $abc = $this->notitle;
        $padding = $this->modalBodyPadding===false? '0' : $this->modalBodyPadding;
        $this->getView()->registerCss("
         .modal-fs .modal-body{
           padding:{$padding};
         }
        ");
+       if($this->notitle){
+        $this->getView()->registerCss("
+        .modal-fs .modal-body{
+          top:5px;
+          padding-top:0px;
+        }
+       ");
+       $this->getView()->registerCss("
+        .modal-fs .modal-header{
+          display:none;
+        }
+       ");
+       }
 
         parent::init();
 
